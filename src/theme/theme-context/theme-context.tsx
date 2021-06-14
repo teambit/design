9d01ba016baa74b8d281ca9568ae4a9041ab7656
Uri Kutner <urikutner@gmail.com>
2021-06-14 13:09:22 +0300

    move comps to src

--- src/theme/theme-context/theme-context.tsx
+++ src/theme/theme-context/theme-context.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import 'reset-css';
 import { Theme } from '@teambit/base-ui.theme.theme-provider';
 import { Roboto } from '@teambit/base-ui.theme.fonts.roboto';
-import { IconFont } from '@teambit/design.theme.icon-font';
+import { IconFont } from '@teambit/design.theme.icons-font';
 import sizes from './sizes.module.scss';
 import global from './global.module.scss'; // TODO - rename
 import colors from './colors.module.scss';
@@ -12,8 +12,10 @@ export type ThemeContextProps = {} & HTMLAttributes<HTMLDivElement>;
 
 export function ThemeContext({ children, className, ...rest }: ThemeContextProps) {
   return (
+    // TODO: @kutner fix that
+    // @ts-ignore
     <Theme {...rest} className={classNames(className, sizes.heading, global.overrides, colors.status)}>
-      <IconFont />
+      <IconFont query="eo46cx" />
       <Roboto />
       {children}
     </Theme>
