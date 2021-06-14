db0986d4f060b04738b430a56debedcf09a9803a
Uri Kutner <urikutner@gmail.com>
2021-06-14 13:14:58 +0300

    import design button

---
+++ src/ui/icon-button/icon-button.compositions.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import { colorPalette } from '@teambit/base-ui.theme.accent-color';
+import { IconButton } from './icon-button';
+
+export function IconButtonExample() {
+  return (
+    <IconButton id="icon-and-text" icon="error-circle">
+      Terminate
+    </IconButton>
+  );
+}
+
+export function IconButtonDisabled() {
+  return (
+    <IconButton id="disabled-icon-button" icon="error-circle" disabled>
+      Terminate
+    </IconButton>
+  );
+}
+
+export function IconOnlyButtonExample() {
+  return <IconButton id="icon-only" icon="error-circle"></IconButton>;
+}
+
+export function TextOnlyButtonExample() {
+  return <IconButton>Terminate</IconButton>;
+}
+
+export function LoadingButtonExample() {
+  return <IconButton loading />;
+}
+
+export function ActiveButtonExample() {
+  return <IconButton active>Terminate</IconButton>;
+}
+
+export function ActiveWithIconButtonExample() {
+  return (
+    <IconButton active icon="error-circle">
+      Terminate
+    </IconButton>
+  );
+}
+
+export function DefaultButton() {
+  return <IconButton priority="ghost">Update</IconButton>;
+}
+
+export function GhostDisableButton() {
+  return (
+    <IconButton priority="ghost" disabled>
+      Update
+    </IconButton>
+  );
+}
+
+export function CtaButton() {
+  return <IconButton priority="cta">Save</IconButton>;
+}
+
+export function CtaButtonWithIcon() {
+  return (
+    <IconButton priority="cta" icon="download">
+      Save
+    </IconButton>
+  );
+}
+
+export function LargeCtaButton() {
+  return (
+    <IconButton priority="cta" size="l">
+      Save
+    </IconButton>
+  );
+}
+
+export function CtaDisableButton() {
+  return (
+    <IconButton priority="cta" disabled>
+      Delete
+    </IconButton>
+  );
+}
+
+export function DangerButton() {
+  return (
+    <IconButton priority="cta" className={colorPalette.error}>
+      Delete
+    </IconButton>
+  );
+}
+
+export function LargeDangerButton() {
+  return (
+    <IconButton priority="cta" size="l" className={colorPalette.error}>
+      Delete
+    </IconButton>
+  );
+}
+
+export function DangerDisabledButton() {
+  return (
+    <IconButton priority="cta" disabled className={colorPalette.error}>
+      Delete
+    </IconButton>
+  );
+}
+
+export function SuccessButton() {
+  return (
+    <IconButton priority="cta" className={colorPalette.success}>
+      Saved!
+    </IconButton>
+  );
+}
+
+// export function WarningButton() {
+//   return (
+//     <IconButton priority="cta" className={colorPalette.warning}>
+//       Make Admin?
+//     </IconButton>
+//   );
+// }
---
+++ src/ui/icon-button/icon-button.spec.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { IconButtonExample, IconButtonDisabled } from './icon-button.compositions';
+
+
+describe('IconButton', () => {
+    it('should render the text in the button', () => {
+      const { container } = render(<IconButtonExample />);
+      expect(container.querySelector('#icon-and-text').textContent).toEqual('Terminate');
+    });
+    it('should render a disabled button', () => {
+      const { container } = render(<IconButtonDisabled />);
+      expect(container.querySelector('#disabled-icon-button')).toHaveProperty('disabled');
+    });
+  });
