98cfa28e39687bd03453abd084366e76b51358c1
Uri Kutner <urikutner@gmail.com>
2021-06-14 20:45:19 +0300

    icon component

---
+++ src/ui/elements/icon/icon.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { BaseIcon, BaseIconProps } from '@teambit/base-ui.elements.icon';
+
+const iconSetPrefix = 'bitcon';
+
+export type IconProps = BaseIconProps;
+
+/**
+ * Placeholder for an icon, from Bit.dev's icon font.
+ *
+ * @name Icon
+ * @example
+ * // Embed icon at the document root:
+ * <EvaIconFont query="sdfj4k2d"/>
+ *
+ * // Then use icon anywhere in the document:
+ * <Icon of="spinner"/>
+ */
+export function Icon({ className, of: iconName, ...rest }: IconProps) {
+  return (
+    <BaseIcon
+      of={`${iconSetPrefix}-${iconName}`}
+      className={className}
+      data-bit-id="teambit.evangelist/elements/icon"
+      {...rest}
+    />
+  );
+}
