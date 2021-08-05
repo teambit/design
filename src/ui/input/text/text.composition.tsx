bc1e148fa4d819cc7295422a22a6abd4ff25292c
Josh Kuttler <yoshuakuttler@gmail.com>
2021-08-05 18:57:44 +0300

    add sizes and darkmode compositions

--- src/ui/input/text/text.composition.tsx
+++ src/ui/input/text/text.composition.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { darkMode } from '@teambit/base-ui.theme.dark-theme';
 import { Text, TextArea } from './text';
 
 export const InputExample = () => <Text placeholder="enter text..." data-testid="test-input" />;
@@ -12,6 +13,16 @@ export const SuccessInput = () => <Text success data-testid="test-input" />;
 export const DisabledInput = () => {
   return <Text placeholder="placeholder..." disabled data-testid="test-input" />;
 };
+
+export const SmallInput = () => <Text style={{ fontSize: 12 }} placeholder="enter text..." data-testid="test-input" />;
+
+export const BigInput = () => <Text style={{ fontSize: 24 }} placeholder="enter text..." data-testid="test-input" />;
+
+export const InputDarkMode = () => (
+  <div className={darkMode} style={{ padding: '20px 40px', backgroundColor: '#0c0c0c' }}>
+    <Text placeholder="enter text..." data-testid="test-input" />
+  </div>
+);
 
 export const TextAreaExample = () => <TextArea placeholder="multiline text..." data-testid="test-textarea" />;
 
