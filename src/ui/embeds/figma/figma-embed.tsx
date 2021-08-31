a1fe1ebb17d89c4682de9b05ea10101cdc5f960e
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-08-31 15:23:44 +0200

    modify default props

--- src/ui/embeds/figma/figma-embed.tsx
+++ src/ui/embeds/figma/figma-embed.tsx
@@ -4,14 +4,13 @@ import styles from './figma-embed.module.scss';
 
 export type FigmaEmbedProps = {} & React.IframeHTMLAttributes<HTMLIFrameElement>;
 
-export function FigmaEmbed({ loading = 'lazy', width = '100%', height = '600', className, ...rest }: FigmaEmbedProps) {
-  return (
-    <iframe
-      {...rest}
-      className={classNames(styles.iframe, className)}
-      width={width}
-      height={height}
-      loading={loading}
-    ></iframe>
-  );
+export function FigmaEmbed({ className, ...rest }: FigmaEmbedProps) {
+  return <iframe {...rest} className={classNames(styles.iframe, className)}></iframe>;
 }
+
+FigmaEmbed.defaultProps = {
+  allowfullscreen: true,
+  loading: 'lazy',
+  width: '100%',
+  height: '600px',
+};
