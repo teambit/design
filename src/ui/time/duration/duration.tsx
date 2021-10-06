22ce2c00c5a7c7fbedcdf42bcf0bd2539cbf4180
Uri Kutner <urikutner@gmail.com>
2021-10-06 16:01:23 +0300

    remove legacy dependencies with design scope

--- src/ui/time/duration/duration.tsx
+++ src/ui/time/duration/duration.tsx
@@ -1,13 +1,12 @@
 import React, { HTMLAttributes } from 'react';
 import classNames from 'classnames';
 import { Duration } from '@teambit/base-ui.time.duration';
-import type { Duration as TimeDuration } from '@teambit/toolbox.date.duration';
 import { BaseIcon } from '@teambit/base-ui.elements.icon';
 import { ellipsis } from '@teambit/design.ui.styles.ellipsis';
 import styles from './duration.module.scss';
 
 export type DurationUiProps = {
-  time?: TimeDuration | number;
+  time?: number;
 } & HTMLAttributes<HTMLDivElement>;
 
 export function DurationUi({ time, className, ...rest }: DurationUiProps) {
