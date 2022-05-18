91cada4d706db0d859e1711ec449d92a14db18be
Josh Kuttler <yoshuakuttler@gmail.com>
2022-05-18 23:00:45 +0300

    Checkout latest and use new theme provider in design env preview

--- src/envs/react/react.preview.runtime.ts
+++ src/envs/react/react.preview.runtime.ts
@@ -1,6 +1,6 @@
 import { PreviewRuntime } from '@teambit/preview';
 import { ReactAspect, ReactPreview } from '@teambit/react';
-import { ThemeContext } from '@teambit/design.theme.theme-context';
+import { BaseTheme } from '@teambit/design.themes.base-theme';
 import { DesignReactEnvAspect } from './react-env.aspect';
 
 export class DesignReactEnvPreview {
@@ -11,7 +11,7 @@ export class DesignReactEnvPreview {
   static async provider([react]: [ReactPreview]) {
     const dotComponentsReactPreviewMain = new DesignReactEnvPreview();
     // uncomment the line below to register a new provider to wrap all compositions using this environment with a custom theme.
-    react.registerProvider([ThemeContext]);
+    react.registerProvider([BaseTheme]);
 
     return dotComponentsReactPreviewMain;
   }
