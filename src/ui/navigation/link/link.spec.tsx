4dcfa777c0c5c2a60745f9c742a74b35bc6fea06
Josh Kuttler <yoshuakuttler@gmail.com>
2022-07-07 23:56:52 +0300

    changes

--- src/ui/navigation/link/link.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicLink } from './link.composition';
-
-it('should render a link with correct text', () => {
-  const { getByText } = render(<BasicLink />);
-  const rendered = getByText('Go to bit.dev');
-  expect(rendered).toBeTruthy();
-});
