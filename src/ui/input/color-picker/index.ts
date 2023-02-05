9185c3044121ef7e7bb90530514a4b8dea0f1719
Josh Kuttler <yoshuakuttler@gmail.com>
2023-02-05 19:04:37 +0200

    remove localy color picker

--- src/ui/input/color-picker/index.ts
+++
@@ -1,4 +0,0 @@
-export { ColorPicker, DEFAULT_BACKGROUND_ICON_COLOR, DEFAULT_STRIP_COLOR } from './color-picker';
-export type { ColorPickerProps } from './color-picker';
-export { ColorsBox } from './colors-box';
-export type { ColorsBoxProps } from './colors-box';
